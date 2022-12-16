3a0d82368aff58e7bfd39bd80fd8830d803181c3
CANMOSE <burakbarit@hotmail.com>
2022-12-16 14:00:29 +0300

    kronometre
    kronometre

---
+++ soru2/Kronometre.vb
@@ -0,0 +1,124 @@
+﻿Public Class Kronometre
+    Dim saniye, dakika, saat As Integer
+
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        saniye = saniye + 1
+
+        If saniye = 60 Then
+            dakika = dakika + 1
+            saniye = 0
+        End If
+        If dakika = 60 Then
+            saat = saat + 1
+            dakika = 0
+        End If
+
+        If saat = 24 Then saat = 0
+
+        Label5.Text = saniye
+        Label6.Text = dakika
+        Label7.Text = saat
+
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Timer1.Stop()
+
+        saat = 0
+        dakika = 0
+        saniye = 0
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Timer1.Enabled = False
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Timer1.Enabled = True
+
+    End Sub
+
+
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        saat = 0
+        dakika = 0
+        saniye = 0
+
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Timer1.Start()
+
+    End Sub
+
+    Private Sub Button1_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Timer1.Stop()
+
+    End Sub
+
+    Private Sub Button2_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        ListBox1.Items.Add("Saat")
+        ListBox1.Items.Add(Label7.Text)
+        ListBox1.Items.Add("Dakika")
+        ListBox1.Items.Add(Label6.Text)
+        ListBox1.Items.Add("Saniye")
+        ListBox1.Items.Add(Label5.Text)
+        ListBox1.Items.Add("-----------")
+
+    End Sub
+
+    Private Sub Button5_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        If Button1.Enabled Then
+            saat = 0
+            dakika = 0
+            saniye = 0
+            Label5.Text = 0
+            Label6.Text = 0
+            Label7.Text = 0
+            Timer1.Stop()
+        End If
+
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        ListBox1.Items.Clear()
+
+
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        Using SW As New IO.StreamWriter(TextBox1.Text, True)
+            For Each itm As String In Me.ListBox1.Items
+                SW.WriteLine(itm)
+            Next
+        End Using
+    End Sub
+
+
+    Private Sub Button7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button7.Click
+
+        Dim oku As SaveFileDialog = New SaveFileDialog
+
+        oku.Title = "Dosya Seç"
+
+        oku.ShowDialog()
+        If oku.FileName <> "" Then
+            Dim Yol() As String
+
+            Yol = oku.FileNames
+
+            For Each yolgoster As String In Yol
+
+                TextBox1.Text = yolgoster
+
+            Next
+
+        End If
+    End Sub
+
+End Class
---
+++ soru2/My Project/AssemblyInfo.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Kronometre")> 
+<Assembly: AssemblyDescription("CANMOSE")> 
+<Assembly: AssemblyCompany("CANMOSE")> 
+<Assembly: AssemblyProduct("CANMOSE")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyTrademark("CANMOSE")> 
+
+<Assembly: ComVisible(False)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("9e75aece-0bde-4aaf-8c6f-e6928f508f0a")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
+
+<Assembly: NeutralResourcesLanguageAttribute("tr")> 
